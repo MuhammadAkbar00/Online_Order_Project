d32bf557c24dd8752e3731921ae4a5dc7bb86dff
OrangeeAble <omar_mohamed900@yahoo.com>
2020-03-06 19:24:54 +0300

    updated project

--- jwtRegistrationAPI/jwtRegistration/src/main/java/com/example/demo/jwt/AuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.jwt;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
-
--- jwtRegistrationAPI/jwtRegistration/src/main/java/com/example/demo/jwt/JwtUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.jwt;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JwtUserRepository extends JpaRepository<JwtUser, Long> {
-
-    JwtUser findByUsername(String username);
-}
