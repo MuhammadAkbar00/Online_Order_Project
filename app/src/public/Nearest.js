a7aa56b718c478ea1bcda9b035feced74d285639
OrangeeAble <omar_mohamed900@yahoo.com>
2020-03-14 21:17:13 +0300

    fixed map tile handlign

--- app/src/public/Nearest.js
+++ app/src/public/Nearest.js
@@ -2,13 +2,16 @@ import React, {useEffect, useState} from 'react'
 import 'ol/ol.css'
 import {Map, View, Feature} from 'ol'
 import TileLayer from 'ol/layer/Tile'
-import OSM from 'ol/source/OSM'
+import OSM, {ATTRIBUTION} from 'ol/source/OSM'
 import {Container, Col, Row} from "react-bootstrap"
 import VectorLayer from 'ol/layer/Vector'
 import VectorSource from 'ol/source/Vector'
 import Point from 'ol/geom/Point'
-import {Circle as CircleStyle, Fill, Icon, Stroke, Style} from 'ol/style';
+import {Circle as CircleStyle, Fill, Icon, Stroke, Style, RegularShape} from 'ol/style';
 import Auth from "../auth.js"
+import { toSize } from 'ol/size'
+import Overlay from 'ol/Overlay';
+import TileState from 'ol/TileState';
 
 export default () => {
 
@@ -18,21 +21,64 @@ export default () => {
     //Do this one time and delete after objects references
     useEffect(()=>{
 
+
+        const showPopUp = (element, data)=>{
+            element.innerHTML = `
+            
+                <div style="background-color: black; padding: 10px; border-radius: 10px">
+                    <p>${data}</p>
+                </div>
+            
+            `
+        }
+
+        const hidePopUp = (element) => {
+            element.innerHTML=``
+        }
+
+
+
         if (navigator.geolocation){
             navigator.geolocation.getCurrentPosition(
                 ()=>{setPermission(true)}
             )
             if (permission) {
 
+                let popupElement = document.getElementById('popup');
+
                 let point1 = new Feature({
                     type: 'icon',
                     geometry: new Point([5735186.62973667, 2910517.015046888]),
                 })
 
+                let point2 = new Feature({
+                    type: 'icon',
+                    geometry: new Point([5735186.62973667, 2910610.01]),
+                })
+
+                let popup = new Overlay({
+                    element: popupElement,
+                    positioning: 'bottom-center',
+                    stopEvent: false,
+                    offset: [0, -50]
+                });
+                  
+
+
                 let vectorLayer = new VectorLayer({
                     source: new VectorSource({
-                        features: [point1]
+                        features: [point1, point2]
+                    }),
+                    style: new Style({
+                        image: new Icon({
+                            anchor: [0.5,428],
+                            anchorYUnits: 'pixels',
+                            src: 'images/icon.png',
+                            scale: 0.1,
+                            size: toSize(428)
+                        })
                     })
+
                 })
 
 
@@ -40,7 +86,33 @@ export default () => {
                     target: 'map',
                     layers: [
                         new TileLayer({
-                            source: new OSM()
+                            source: new OSM({
+                                tileLoadFunction: async (tile, src) => {
+                                    let get = await fetch(src,{
+                                        method: 'GET',
+                                        headers: {
+                                            "x-rapidapi-host": "maptiles.p.rapidapi.com",
+                                            "x-rapidapi-key": "e71b8ab174msh1c150bbedd0d83ap1e4990jsn81ff4ea8a289",
+                                        }
+                                    })
+
+                                    let data = await get.blob()
+
+                                    data = await URL.createObjectURL(data)
+                                    
+                                    if (data !== undefined) {
+                                        tile.getImage().src = data;
+                                    } else {
+                                        tile.setState(TileState.ERROR);
+                                    }
+                                    
+                                    setTimeout(() => {
+                                        URL.revokeObjectURL(data)
+                                    }, 1000);
+
+                                },
+                                url: 'https://maptiles.p.rapidapi.com/en/map/v1/{z}/{x}/{y}.png'
+                            })
                         }),
                         vectorLayer
                     ],
@@ -52,11 +124,26 @@ export default () => {
                     })
                 })
 
+                map.addOverlay(popup);
 
+                let clickHandler = (e) => {
+                    let feature = map.forEachFeatureAtPixel(e.pixel,
+                        function(feature) {
+                          return feature
+                        });
+                      let element = popup.getElement()
+                      if (feature) {
+                        let coordinates = feature.getGeometry().getCoordinates()
+                        
+                        popup.setPosition(coordinates)
+                        showPopUp(element, "Al Rayyan branch")
+                      }else{
+                          hidePopUp(element)
+                      }
 
-                // setInterval(()=>{
-                //     console.log(map.getView().getCenter())
-                // },1000)
+                }
+
+                map.on('click', clickHandler)
 
             }
         }
@@ -68,7 +155,7 @@ export default () => {
             <Row>
                 <Col md={6} xs={6}></Col>
                 <Col md={6}>
-                    <div id={"map"} className={"map"}></div>
+                    <div id={"map"} className={"map"}><div id="popup"></div></div>
                 </Col>
             </Row>
         </Container>
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -20,29 +20,29 @@ public class UserController {
 
     @Autowired
     private JwtUserRepository jwtUserRepository;
-    @Autowired
-    private StudentRepository studentRepository;
-    @Autowired
-    private RegisterRepository registerRepository;
+//    @Autowired
+//    private StudentRepository studentRepository;
+//    @Autowired
+//    private RegisterRepository registerRepository;
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping(path = "/students", method = { RequestMethod.GET })
-    public ResponseEntity<?> profiles(Authentication authentication) throws AuthenticationException {
-        System.out.println("profile for " + authentication.getName());
-        Student student = studentRepository.findFirstByName(authentication.getName());
-        System.out.println("data: " + student);
-        return ResponseEntity.ok(student);
-    }
-
-    @RequestMapping(path = "/registrations", method = { RequestMethod.GET })
-    public ResponseEntity<?> registrations(Authentication authentication) throws AuthenticationException {
-        System.out.println("registrations for " + authentication.getName());
-        Student student = studentRepository.findFirstByName(authentication.getName());
-        List<Register> registrations = registerRepository.findByStudentId(student.getId());
-        System.out.println("registrations: " + registrations);
-        return ResponseEntity.ok(registrations);
-    }
+//    @RequestMapping(path = "/students", method = { RequestMethod.GET })
+//    public ResponseEntity<?> profiles(Authentication authentication) throws AuthenticationException {
+//        System.out.println("profile for " + authentication.getName());
+//        Student student = studentRepository.findFirstByName(authentication.getName());
+//        System.out.println("data: " + student);
+//        return ResponseEntity.ok(student);
+//    }
+//
+//    @RequestMapping(path = "/registrations", method = { RequestMethod.GET })
+//    public ResponseEntity<?> registrations(Authentication authentication) throws AuthenticationException {
+//        System.out.println("registrations for " + authentication.getName());
+//        Student student = studentRepository.findFirstByName(authentication.getName());
+//        List<Register> registrations = registerRepository.findByStudentId(student.getId());
+//        System.out.println("registrations: " + registrations);
+//        return ResponseEntity.ok(registrations);
+//    }
 
     @RequestMapping(path = "/users", method = { RequestMethod.GET })
     public ResponseEntity<?> profile(Authentication authentication) throws AuthenticationException {
