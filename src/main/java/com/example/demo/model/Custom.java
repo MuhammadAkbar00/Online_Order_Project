45b3b7a5209eb6f3a3d55f1fb8babb8a29443f37
mahmoud <the-all132@hotmail.com>
2020-03-23 17:06:37 +0300

    Merge branch 'Omar' of https://github.com/Ramo-94/inDine3231 into Mahmoud
     Conflicts:
    	.idea/dataSources.xml
    	.idea/misc.xml
    	app/src/App.js
    	app/src/db.js
    	db/db.lck
    	db/db.properties
    	db/db.script
    	src/main/java/com/example/demo/controller/UserController.java
    	src/main/java/com/example/demo/model/Normal.java
    	src/main/java/com/example/demo/model/Product.java
    	src/main/java/com/example/demo/repository/CourseRepository.java
    	src/main/java/com/example/demo/repository/UserRepository.java
    	target/classes/com/example/demo/controller/MarketingController.class
    	target/classes/com/example/demo/controller/UserController.class
    	target/classes/com/example/demo/model/Advert.class
    	target/classes/com/example/demo/model/Advertiser.class
    	target/classes/com/example/demo/model/Analytics.class
    	target/classes/com/example/demo/model/Coupon.class
    	target/classes/com/example/demo/model/Custom.class
    	target/classes/com/example/demo/model/CustomPart.class
    	target/classes/com/example/demo/model/Experience.class
    	target/classes/com/example/demo/model/Normal.class
    	target/classes/com/example/demo/model/Occasion.class
    	target/classes/com/example/demo/model/Order.class
    	target/classes/com/example/demo/model/OrderItem.class
    	target/classes/com/example/demo/model/Part.class
    	target/classes/com/example/demo/model/Product.class
    	target/classes/com/example/demo/model/User.class
    	target/classes/com/example/demo/repository/CourseRepository.class
    	target/classes/com/example/demo/repository/UserRepository.class

---
+++ src/main/java/com/example/demo/model/Custom.java
@@ -0,0 +1,125 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+public class Custom {
+    private long id;
+    private String name;
+    private String desc;
+    private Date date;
+    private String type;
+    private int total;
+    private Occasion occasion;
+    private Collection<CustomPart> customParts;
+    private Collection<Product> products;
+
+    @Id
+    @Column(name = "ID")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "DESC")
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Basic
+    @Column(name = "DATE")
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Basic
+    @Column(name = "TYPE")
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Basic
+    @Column(name = "TOTAL")
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Custom custom = (Custom) o;
+        return id == custom.id &&
+                total == custom.total &&
+                Objects.equals(name, custom.name) &&
+                Objects.equals(desc, custom.desc) &&
+                Objects.equals(date, custom.date) &&
+                Objects.equals(type, custom.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, desc, date, type, total);
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "OCCASION_ID", referencedColumnName = "ID")
+    public Occasion getOccasion() {
+        return occasion;
+    }
+
+    public void setOccasion(Occasion occasion) {
+        this.occasion = occasion;
+    }
+
+    @OneToMany(mappedBy = "custom")
+    public Collection<CustomPart> getCustomParts() {
+        return customParts;
+    }
+
+    public void setCustomParts(Collection<CustomPart> customParts) {
+        this.customParts = customParts;
+    }
+
+    @OneToMany(mappedBy = "custom")
+    public Collection<Product> getProducts() {
+        return products;
+    }
+    public void setProducts(Collection<Product> products) {
+        this.products = products;
+    }
+
+}
